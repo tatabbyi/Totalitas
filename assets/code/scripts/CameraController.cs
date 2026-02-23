97bcb4bb12083a234098524234bfa8c6f1c2f21f
Tatabbyi <ytrachel0@gmail.com>
2026-02-23 17:25:43 +0000

    fix: updated camera to look at player and stay inside board area

--- assets/code/scripts/CameraController.cs
+++ assets/code/scripts/CameraController.cs
@@ -5,14 +5,28 @@ public class CameraController : MonoBehaviour
     [Header("Target to Follow")]
     public Transform target; //player
     [Header("Camera Settings")]
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public float smoothSpeed = 0.125f; //camera moves to desired position speed
+    public Vector3 offset = new Vector3(-10f, 10f, -10f); //place camera diagonally above and behind 
+    [Header("Rotate Camera")]
 
+    public bool lockRotation = true; //camera fixed if true otherwise rotate to player
+    public Vector3 fixedEulerAngles = new Vector3(25f, 45f, 0f); //rotation angles for iso view
+    private Vector3 velocity; // for tracking movement speed internally
     void LateUpdate() //moves camera to target smoothly
     {
         if (target == null) return;
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        transform.position = Vector3.SmoothDamp(
+            transform.position, //current pos
+            desiredPosition,
+            ref velocity,
+            smoothTime //time it takes to smooth
+        );
+        if (lockRotation){
+            transform.rotation = Quaternion.Euler(fixedEulerAngles); //set rotation for each frame
+        }else{
+           transform.lookAt(target);//alternative camera will always look at the player
+            
+        }
     }
 }
